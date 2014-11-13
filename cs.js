a4b154edffea794fc66ac71189d9187a73ba0b0e
Chun Chien <johnyluyte@gmail.com>
2014-11-13 23:04:06 +0800

    style: tidy up codes

--- cs.js
+++ cs.js
@@ -1,29 +1,31 @@
- /* 
+/* 
+(1)
   This file (cs.js) implements the "automatically apply" function.
   It will 
   - Check if the "auto_apply" variable in chrome.storage.sync is true.
   - Make sure the site is not in the "excluded_lists" using Regex.
   - Create a style node: <style> our_css_rules </style>, append to <header>.
 
-  --
+(2)
   If the user "manually applies" a font setting, we have to disable the <style> node mentioned above.
   
-  When the user "manually applies" a font setting, popup.js will send a "msg_disable_auto_style_node" MESSAGE to this file (cs.js). 
-  The runtime.onMessage.addListener() will receive this MESSAGE and process.
+  When the user "manually applies" a font setting, popup.js will send a "msg_disable_auto_style_node" Message to this file (cs.js). 
+  The runtime.onMessage.addListener() will listen this Message.
 
-  --
+(3)
   Since we have registerd this file (cs.js) as "content_scripts" in manifest.json,
-  this file (cs.js) will be excuted everytime the user refresh a page, visits a page or creates a tab as long as ClearJC is enabled in Chrome plug-in.
+  this file (cs.js) will be excuted everytime the user refreshes a page, visits a page or creates a tab as long as ClearJC is enabled in Chrome plug-in.
 
-  If we use any 3rd-party API in this file (cs.js), we should also register them in manifest.json.
+  As a result, if we use any 3rd-party API in this file (cs.js), we should also register them in manifest.json.
   
-  So, for simplicity, instead of using jQuery, we use basic DOM here.
+  So, for simplicity, we use native (vanilla) javascript DOM here.
   e.g. 
     window.onload();
     document.getElementsByTagName();
 */
 
 ID_OF_AUTO_INJECTION_NODE = "clearJC_auto_style";
+
 
 window.onload = function(){
   /* Check if the user want ClearJC to automatically inject CSS on every page. */
@@ -38,17 +40,12 @@ window.onload = function(){
 }
 
 
-function checkIfAutoInjectIsOn(){
-
-}
-
-/* We will not inject CSS to URLs that are excluded by the User*/
-// TODO: 
-// coursera, http://dictionary.goo.ne.jp/
+/* We should not inject CSS to URLs that are in the "excluded lists" */
+// TODO: customized REGEX filter
 function isValidURL(url){
   console.debug("[content_script isValidURL()] url=" + url);
-  /* For example, if the URL "https://github.com/" is in the User's excluded list: */
   // Remember, if we use jQuery here, we should update manifest.json to include jQuery in the "content_scripts" field.
+  /* For example, if the URL "https://github.com/" is in the User's excluded lists: */
   if(url.match(/http[s]?:\/\/*github.com\/*/)){
     return false;
   }
@@ -62,18 +59,18 @@ function isValidURL(url){
    and append it to the header of the page.
 */
 function autoInjectCSS(css_code){
-  // TODO: code refactory
+  var node = document.createTextNode(css_code);
   var style = document.createElement("style");
   style.id = ID_OF_AUTO_INJECTION_NODE;
-  var node=document.createTextNode(css_code);
   style.appendChild(node);
-  document.getElementsByTagName("head")[0].appendChild(style);  
+  document.getElementsByTagName("head")[0].appendChild(style);
   console.debug("[content_script autoInjectCSS()]" + css_code);
-  /* Activated 的時候，是不是應該要在網址列那邊顯示一個tip圖案，提示使用者"目前這個網頁的字體有被優化" */
+  // TODO: 自動套用時，是不是應該在網址列後方顯示一個icon，提示使用者"目前這個網頁自動套用ClearJC"
 }
 
 /*
 Diable CSS style inserted by autoInjectCSS() when receive message from popup.js
+Please see the descriptions at the top of this file.
 */
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.text && (msg.text == "msg_disable_auto_style_node")) {
